5877d710ab32e992f0cb5daf050190201c4fe424
Melissa Bower <melissaclausen@u.boisestate.edu>
2017-03-28 20:02:57 -0600

    Adding updates

--- contact.php
+++ contact.php
@@ -5,6 +5,7 @@
     <form method ="POST" action="handler.php">
     <input type = "text" name = "email">
     <input type = "text" name ="input">
+    <input type="submit">   
     </form>
     <p>A form will be here for the user to contact the photographer.</p>
 </div>
