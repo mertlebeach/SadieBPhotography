3143616b88523842649119ae37ee574ef0456c0a
Melissa Bower <melissaclausen@u.boisestate.edu>
2017-03-30 10:09:34 -0600

    Adding updates

--- classes/Dao.php
+++ classes/Dao.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once 'KLogger.php';
 
 class Dao {
     
@@ -9,23 +10,29 @@ private $host = "us-cdbr-iron-east-03.cleardb.net";
 private $db = "heroku_2f4decd2ae70db4";
 private $user = "bfbdbf41253232";
 private $pass = "0950cb64";
- 
+private $log;
+  public function __construct () {
+    $this->log = new KLogger ("/tmp/log.txt" , KLogger::WARN);
+  }
+
   public function getConnection () {
-     
+    $this->log->LogDebug("Trying to get a database connection...");
     try {
       $conn = new PDO("mysql:host={$this->host};dbname={$this->db}", $this->user,
             $this->pass);
     } catch (Exception $e) {
-          echo "issue with the connections";
+      $this->log->LogFatal($e);
       exit;
     }
- 
+    $this->log->LogDebug("Got a database connection");
     return $conn;
   }
   
   public function save($email, $input) {
  
-      echo($email);
+    $this->log->LogInfo("Saving email: " . $email);
+    $this->log->LogInfo("Saving input: " . $input);
+      echo($email)
     $conn = $this->getConnection();
     $saveQuery = "insert into inquiry (inquiry, email) values (:input, :email)";
     $q = $conn->prepare($saveQuery);
--- handler.php
+++ handler.php
@@ -6,14 +6,29 @@
 
 
 $email= htmlentities($_POST['email']);
-$info= htmlentities($_POST['info']);
+$input= htmlentities($_POST['info']);
 $username= htmlentities($_POST['username']);
-$password= htmlentities($_POST['password']);    
+$password= htmlentities($_POST['passw']);    
 
+
+  if (filter_var($email, FILTER_VALIDATE_EMAIL)!=true) {
+    $_SESSION['message'] = "Invalid email: Please try again!";
+      
+    $_SESSION['mtype'] = 'bad';
+    $_SESSION['inputs'] = $_POST;
+  
+    header("Location:contact.php");
+ 
+    exit;
+  } else {
+    unset($_SESSION['inputs']);
+    $_SESSION['message'] = "Thanks for your inquiry! We will get back to you as soon as possible!";
+    $_SESSION['mtype'] = 'good';
+  }
 
 
     $dao->save($email,$input);
 
 header("Location:contact.php");
 
-end;
+exit;
