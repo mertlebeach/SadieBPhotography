c4a8e8de15ff34bde71a0dd0271add3fc2d7cf6d
Melissa Bower <melissaclausen@u.boisestate.edu>
2017-03-30 09:08:28 -0600

    Adding updates

--- handler.php
+++ handler.php
@@ -11,21 +11,6 @@ $username= htmlentities($_POST['username']);
 $password= htmlentities($_POST['password']);    
 
 
-  if (filter_var($email, FILTER_VALIDATE_EMAIL)!=true) {
-    $_SESSION['message'] = "Invalid email: Please try again!";
-      
-    $_SESSION['mtype'] = 'bad';
-    $_SESSION['inputs'] = $_POST;
-  
-    header("Location:contact.php");
- 
-    exit;
-  } else {
-    unset($_SESSION['inputs']);
-    $_SESSION['message'] = "Thanks for your inquiry! We will get back to you as soon as possible!";
-    $_SESSION['mtype'] = 'good';
-  }
-
 
     $dao->save($email,$input);
 
