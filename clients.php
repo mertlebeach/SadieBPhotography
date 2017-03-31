1dc11eca8e55b66e25aa6aa2837a969ed0e437c7
Melissa Bower <melissaclausen@u.boisestate.edu>
2017-03-30 23:07:58 -0600

    Adding clients update

--- clients.php
+++ clients.php
@@ -6,7 +6,7 @@
 ?>
 <div class="clients">
  
-  <h1>Admin Log in</h1>
+  <h1>Admin Log on</h1>
   <form method="POST" action="lhandler.php"  enctype="multipart/form-data">
      <label for="comment">Username:</label>
     <input type="text" id="username" value="<?php echo $_SESSION['inputs']['username']; ?> " name="username">
@@ -15,7 +15,16 @@
  
     <input type="submit" value="Log in">
   </form>
-
+  <?php
+  if (isset($_SESSION['message'])) {?>
+  <div id="message" class="<?php echo $_SESSION['mtype']; ?>">
+    <?php
+      echo $_SESSION['message'];
+      unset($_SESSION['message']);
+      unset($_SESSION['inputs']);
+    ?>
+       <?php } ?>
+  </div>
 </div>
 <?php
     include('footer.php');
