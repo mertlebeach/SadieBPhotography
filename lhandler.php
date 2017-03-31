c397333368e1a769ad4ebf13c6608be22a2cdc6f
Melissa Bower <melissaclausen@u.boisestate.edu>
2017-03-30 23:24:15 -0600

    Adding clients update

--- lhandler.php
+++ lhandler.php
@@ -3,7 +3,7 @@
 
   require_once 'classes/Dao.php';
   $dao = new Dao();
-echo("hello!!! WHy aren't you there");
+//echo("hello!!! WHy aren't you there");
 
 
 $username= htmlentities($_POST['username']);
@@ -12,7 +12,7 @@ $password= htmlentities($_POST['password']);
 
 
 
-    $dao->is_password_correct($username,$password);
+  //  $dao->is_password_correct($username,$password);
 
     $dao-> does_username_exist($username);
 
