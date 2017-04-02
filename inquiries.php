9fd43a0a1ee60285cde2f7143dfd724c075a4d01
Melissa Bower <melissaclausen@u.boisestate.edu>
2017-04-02 14:32:29 -0600

    Getting rid of admin on inquiries top.

--- inquiries.php
+++ inquiries.php
@@ -5,8 +5,7 @@ $dao->ensure_logged_in();
 
 ?>
 <div class="clients">
-  <h1> <?php echo($_SESSION["name"]);?></h1>
-  <h1>Inquires</h1>
+   <h1>Inquires</h1>
    <?php Render::renderTable($dao->getComments()); ?>
 
   </div>
